2f0e4a105275b43a60466bb0fb8483d3ccd4626b
xxkj2018-spec <xxkj2018@gmail.com>
2026-01-01 09:22:21 +0800

    Supajimi
    “修复白屏配置”。

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 下面这一行是为了解决 GitHub Pages 白屏问题的
+  // 这里的 '/nexus-academica/' 对应你的仓库名
+  base: "/nexus-academica/",
 })
-
-
-
-
-
